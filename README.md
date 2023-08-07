8a996c0721959a4090a88f0b2e698ec0709dea4a
Pau <141568509+Paussito@users.noreply.github.com>
2023-08-07 13:29:31 +0200

    Hi!

---
+++ README.md
@@ -0,0 +1,7 @@
+# How to Make Vars Correctly
+
+Welcome to the **How to Make Vars Correctly** repository! This repository is designed to help you learn and practice how to make Vars!
+
+## Table of Contents
+
+- [How to Make Vars?](/vars.js)
---
+++ vars.js
@@ -0,0 +1,4 @@
+//In this repository we'll see how to make vars correctly;
+
+let n = "Text1 With very important Information" //❌ You mustn't make vars with these types of names, they don't provide information when you're programming.
+let info1 = "Text2 With very important Information" //✅ You must name your vars following this form, then you can know what are they for!
